b19382b98eaca86ad9aa650188a6b8a18f80a0b7
koide <c0a22059c9@edu.teu.ac.jp>
2023-10-03 16:50:33 +0900

    演習１仮

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -32,9 +32,20 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img = pg.transform.rotozoom(kk_img,0,2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
+    kk_imgs ={
+        (0,-5): pg.transform.flip(pg.transform.rotozoom(kk_img,90,1.0),True,True),#下向き
+        (0,5) : pg.transform.rotozoom(kk_img,45,1.0),#上向き
+        (-5,0) : pg.transform.flip(pg.transform.rotozoom(kk_img,0,1.0),False,False),#左向き
+        (5,0) : pg.transform.flip(pg.transform.rotozoom(kk_img,0,1.0),True,False),#右向き
+        (-5,-5):pg.transform.rotozoom(kk_img,-45,1.0),#右下斜め
+        (5,-5): pg.transform.flip(pg.transform.rotozoom(kk_img,-45,1.0),True,True),#左下斜め
+        (-5,5): pg.transform.flip(pg.transform.rotozoom(kk_img,45,1.0),False,False),#右上斜め
+        (5.5):pg.transform.flip(pg.transform.rotozoom(kk_img,45,1.0),True,True)
+    }
+
     """ばくだん"""
     bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
     bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
@@ -42,7 +53,14 @@ def main():
     bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
-    vx, vy = +5, +5  # 練習２：爆弾の速度
+    for i in range (1,60):
+        avx = 1*i
+        avy = 1*i
+        if avx == 100000:
+            i = 1
+            avx = 1
+            avy = 1
+    vx, vy = avx,avy  # 練習２：爆弾の速度
 
     clock = pg.time.Clock()
     tmr = 0
@@ -60,13 +78,43 @@ def main():
         """こうかとん"""
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
+        screen.blit(bg_img, [0, 0])
+
+    # キー入力を処理して画像を変更
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
         for key, mv in delta.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
                 sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
+
         kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
-        if check_bound(kk_rct) != (True,True): #練習４：はみ出し判定
+        if check_bound(kk_rct) != (True, True):  # 練習４：はみ出し判定
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
+         # キーに応じた画像を表示
+        if sum_mv[0] > 0:
+            kk_img = kk_imgs[(5, 0)]
+        elif sum_mv[0] < 0:
+            kk_img = kk_imgs[(-5, 0)]
+        elif sum_mv[1] > 0:
+            kk_img = kk_imgs[(0, 5)]
+        elif sum_mv[1] < 0:
+            kk_img = kk_imgs[(0, -5)]
+        
+        elif (sum_mv[0] > 0 and sum_mv[1] > 0) or (sum_mv[1] > 0 and sum_mv[0] > 0):
+            kk_img = kk_imgs[(5,5)]
+        elif (sum_mv[0] > 0 and sum_mv[1] < 0) or (sum_mv[1] > 0 and sum_mv[0] < 0):
+            kk_img = kk_imgs[(5,-5)]
+        elif (sum_mv[0] < 0 and sum_mv[1] > 0) or (sum_mv[1] > 0 and sum_mv[0] < 0):
+            kk_img = kk_imgs[(-5,5)]
+        elif (sum_mv[0] < 0 and sum_mv[1] < 0) or (sum_mv[1] < 0 and sum_mv[0] < 0):
+            kk_img = kk_imgs[(-5,-5)]
+
+        else:
+            kk_img = kk_imgs[(-5, 0)]  # デフォルト画像（左向き）
+
         screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
         """"ばくだん"""
         bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
